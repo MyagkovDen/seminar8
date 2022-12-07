b8c2ac8304f154fe715a5c912e735104dc34c32e
Denis Myagkov <tomich-84@mail.ru>
2022-12-07 23:53:19 +0300

    add sem08sum54

---
+++ sem08sum54/Program.cs
@@ -0,0 +1,61 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки 
+двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+Console.Clear();
+
+int[,] array = GetArray(4, 16, 1, 100);
+PrintArray(array);
+Console.WriteLine();
+DecreaseAtRow(array);
+PrintArray(array);
+
+void DecreaseAtRow(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1) - 1; j++)
+        {
+            int max = j;
+            for (int k = j + 1; k < arr.GetLength(1); k++)
+            {
+                if (arr[i, k] > arr[i, max]) max = k;
+            }
+            int temp = arr[i, j];
+            arr[i, j] = arr[i, max];
+            arr[i, max] = temp;
+        }
+    }
+}
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
